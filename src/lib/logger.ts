97dd80710a8cb20b7d3c5afc1e1542526b4cabc8
Ritikayogi <yogiritika02@gmail.com>
2026-06-24 19:19:16 +0530

    Fix logger TypeScript error for catch variables

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,19 +1,19 @@
 type LogLevel = "info" | "warn" | "error";
 class Logger {
-  private logWithLevel(level: LogLevel, message: string, args?: string | object | "") {
+  private logWithLevel(level: LogLevel, message: string, args?: any) {
     // eslint-disable-next-line no-console
     console[level](`[${level.toUpperCase()}] ${message} ${args ? args : ""}`);
   }
 
-  info(message: string, args?: string | object) {
+  info(message: string, args?: any) {
     this.logWithLevel("info", message, args);
   }
 
-  warn(message: string, args?: string | object) {
+  warn(message: string, args?: any) {
     this.logWithLevel("warn", message, args);
   }
 
-  error(message: string, args?: string | object) {
+  error(message: string, args?: any) {
     this.logWithLevel("error", message, args);
   }
 }
