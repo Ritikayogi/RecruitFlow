2e81253bdca29023ed2bab224007c142d6503c50
Ritikayogi <yogiritika02@gmail.com>
2026-06-24 19:25:53 +0530

    Fix: Import toast from sonner in details.tsx

--- src/components/dashboard/interview/create-popup/details.tsx
+++ src/components/dashboard/interview/create-popup/details.tsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import FileUpload from "../fileUpload";
+import { toast } from "sonner";
 
 interface Props {
   open: boolean;
